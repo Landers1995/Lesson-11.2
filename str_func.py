71d49a4189d58951d092426cf19dc2bc19076615
Landers <il-travkin@yandex.ru>
2024-04-01 22:26:11 +0300

    add docstring to str_func

--- str_func.py
+++ str_func.py
@@ -1,2 +1,16 @@
 def str_upper(some_str):
+    """
+    Return 'some_str' in upper case
+    :param some_str:
+    :return:
+    """
     return some_str.upper()
+
+
+def str_title(some_str):
+    """
+    Return 'some_str' in title case
+    :param some_str:
+    :return:
+    """
+    return some_str.title()
